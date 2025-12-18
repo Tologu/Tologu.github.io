737280b1020fc7a8e2320441dbcd43315fb3ce1e
Tologu <tomaslopezgu@gmail.com>
2025-12-18 10:04:57 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "REEMPLAZA_AQUI",
+  authDomain: "REEMPLAZA_AQUI",
+  projectId: "REEMPLAZA_AQUI",
+  storageBucket: "REEMPLAZA_AQUI",
+  messagingSenderId: "REEMPLAZA_AQUI",
+  appId: "REEMPLAZA_AQUI"
+};
